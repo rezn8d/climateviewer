7420a496803c04ba0706c4d5325da9c51c1d327e
SeH <1s1e1h1@gmail.com>
2015-06-03 01:29:48 -0400

    programming doesnt have to be all about confronting ridiculous stupid shit that other people arbitraiily decided to do

--- proxy.php
+++ proxy.php
@@ -1,10 +1,19 @@
 <?php
-$url = $_POST["url"];
-header("Access-Control-Allow-Origin: *");
+
+//$url = 'http://' . $_GET["url"];
+$url = urldecode($_SERVER['QUERY_STRING']);
+
+
+//http://stackoverflow.com/questions/11733876/how-to-get-content-ot-remote-html-page
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 $output = curl_exec($curl);
 curl_close($curl);
+header("Access-Control-Allow-Origin: *");
+
 echo $output;
+
+
 ?>
+
